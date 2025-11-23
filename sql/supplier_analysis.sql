bc43f4be07f3aa5678134f4caeb3c819003dd716
kandalabalaji <kandalabalaji333@gmail.com>
2025-11-23 18:40:09 +0530

    Add supplier status analysis for 2025

--- sql/supplier_analysis.sql
+++ sql/supplier_analysis.sql
@@ -1,1 +1,21 @@
+WITH supplier_years AS (
+    SELECT supplier_name, year
+    FROM shipments
+    GROUP BY supplier_name, year
+),
+active_2025 AS (
+    SELECT supplier_name FROM supplier_years WHERE year = 2025
+),
+historical AS (
+    SELECT supplier_name FROM supplier_years WHERE year < 2025
+)
+SELECT
+    h.supplier_name,
+    CASE 
+        WHEN a.supplier_name IS NOT NULL THEN 'ACTIVE'
+        ELSE 'CHURNED'
+    END AS status
+FROM historical h
+LEFT JOIN active_2025 a
+ON h.supplier_name = a.supplier_name;
 
